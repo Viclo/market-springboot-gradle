36b0f94157700c512b78fe4aa31e50eb58ad7a37
VicLo <ivangra85@gmail.com>
2023-03-03 05:35:49 -0400

    Implemented JWT, authentication, configuration and user DAO.

---
+++ src/main/java/com/backend/market/web/controller/AuthenticationController.java
@@ -0,0 +1,37 @@
+package com.backend.market.web.controller;
+
+import com.backend.market.domain.dto.AuthenticationRequest;
+import com.backend.market.persistence.dao.UserDao;
+import com.backend.market.web.security.JwtUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+  private final AuthenticationManager authenticationManager;
+  private final UserDao userDao;
+  private final JwtUtils jwtUtils;
+
+  @PostMapping("/authenticate")
+  public ResponseEntity<String> authenticate(@RequestBody AuthenticationRequest authenticationRequest) {
+    authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(authenticationRequest.getEmail(), authenticationRequest.getPassword())
+    );
+    final UserDetails userDetails = userDao.findUserByEmail(
+        authenticationRequest.getEmail());
+    if (userDetails != null) {
+      return ResponseEntity.ok(jwtUtils.generateToke(userDetails));
+    }
+    return  ResponseEntity.status(400).body("Some error has occurred.");
+  }
+}
---
+++ src/main/java/com/backend/market/web/security/JwtAthFilter.java
@@ -0,0 +1,54 @@
+package com.backend.market.web.security;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+import com.backend.market.persistence.dao.UserDao;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAthFilter extends OncePerRequestFilter {
+
+  private final UserDao userDao;
+  private final JwtUtils jwtUtils;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+
+    final String authHeader = request.getHeader(AUTHORIZATION);
+    final String userEmail;
+    final String jwtToken;
+
+    if (authHeader == null || !authHeader.startsWith("Bearer")) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+
+    jwtToken = authHeader.substring(7);
+    userEmail = jwtUtils.extractUsername(jwtToken);
+
+    if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+      UserDetails userDetails = userDao.findUserByEmail(userEmail);
+
+      if (jwtUtils.isTokenValid(jwtToken, userDetails)) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+      }
+    }
+
+    filterChain.doFilter(request, response);
+  }
+}
